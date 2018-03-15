ff0afb5998ac3a27478b03f7966e343f992cbb79
Magesh <tkmagesh77@gmail.com>
2018-03-15 10:42:37 +0800

    using Input decorator

--- first-app/src/app/calculator/calculatorResult.component.ts
+++ first-app/src/app/calculator/calculatorResult.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
 	selector : 'app-calculator-result',
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class CalculatorResultComponent{
 	
+	@Input()
+	data : number = 0;	
 }
