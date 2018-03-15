d888b6e382f33af110039bcedffba43d8bffddac
Magesh <tkmagesh77@gmail.com>
2018-03-15 10:18:16 +0800

    Refactoring calculator result into another component

---
+++ first-app/src/app/calculator/calculatorResult.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector : 'app-calculator-result',
+	templateUrl : 'calculatorResult.component.html',
+	styleUrls : ['calculatorResult.component.css']
+})
+export class CalculatorResultComponent{
+	
+}
