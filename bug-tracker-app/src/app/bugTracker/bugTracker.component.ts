937ad21c8c35439d98e68001ce557ee3b49feb6c
Magesh <tkmagesh77@gmail.com>
2018-03-15 14:06:07 +0800

    toggling and removing closed bugs

--- bug-tracker-app/src/app/bugTracker/bugTracker.component.ts
+++ bug-tracker-app/src/app/bugTracker/bugTracker.component.ts
@@ -16,4 +16,24 @@ export class BugTrackerComponent{
 		};
 		this.bugs.push(newBug);
 	}
+
+	onBugNameClick(bug : Bug){
+		bug.isClosed = !bug.isClosed;
+	}
+
+	onRemoveClosedClick(){
+		for(let index = this.bugs.length-1; index >= 0; index--){
+			if (this.bugs[index].isClosed)
+				this.bugs.splice(index, 1);
+		}
+	}
+
+	getClosedCount(){
+		let closedCount = 0;
+		for(let index = 0, count = this.bugs.length; index < count; index++){
+			if (this.bugs[index].isClosed)
+				++closedCount;
+		}
+		return closedCount;
+	}
 }
